36413fb2737a7ae614b49ccddfd98ecc502eec7c
Weston Rainer <Rainerw1@mymail.nku.edu>
2018-10-10 18:54:39 -0400

    Added the RecyclerView
    Just adds a RecyclerView

--- app/src/main/java/org/stream/group5/streamingapp/MainActivity.java
+++ app/src/main/java/org/stream/group5/streamingapp/MainActivity.java
@@ -1,8 +1,5 @@
 package org.stream.group5.streamingapp;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,16 +7,24 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private BottomNavigationView mBottomNavigationView;
+    private final LinkedList<String> musicList = new LinkedList<>();
+    private RecyclerView recyclerView;
+    private MusicListAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,6 +74,15 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+
+        //RecyclerView
+        for (int x = 0; x < 20; x++)
+            musicList.addLast("Song" + x);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        adapter = new MusicListAdapter(this, musicList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
     @Override
